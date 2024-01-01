a7ffe0434aa5bc0db1d77dda84fc089484798af9
javad yosuefi <javadyousefi9000@gmail.com>
2023-12-31 15:19:59 -0900

    add comments

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -14,8 +14,10 @@ type hint = {
 type InfoBoxProps = warning | hint;
 
 const InfoBox = (props: InfoBoxProps) => {
+  // commen props
   const { children, mode } = props;
 
+  // render jsx base on mode props
   if (mode === "warn") {
     const { title } = props;
 
@@ -29,6 +31,7 @@ const InfoBox = (props: InfoBoxProps) => {
     );
   }
 
+  //   default render
   return (
     <>
       <aside className="flex flex-col items-center justify-center p-2 text-yellow-300 border border-yellow-300 rounded-md gap-y-2">
