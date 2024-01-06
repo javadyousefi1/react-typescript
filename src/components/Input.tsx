2b73b74a075f32b2e21e7d7e5243c0a27d36f0ff
javad yosuefi <javadyousefi9000@gmail.com>
2024-01-06 09:31:30 -0900

    add component props with out ref for input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,22 @@
+import { type ComponentPropsWithoutRef } from "react";
+
 type InputProps = {
   id: string;
   label: string;
-};
+} & ComponentPropsWithoutRef<"input">;
 
-const Input = ({ id, label }: InputProps) => {
+const Input = ({ id, label, ...props }: InputProps) => {
   return (
-    <p className="flex flex-col gap-y-3">
-      <label className="text-main_white" htmlFor={id}>{label} : </label>
-      <input id={id} type="text" className="px-3 py-2 font-bold rounded-lg outline-none"/>
+    <p className="flex flex-col mt-3 gap-y-3">
+      <label className="text-main_white" htmlFor={id}>
+        {label} :{" "}
+      </label>
+      <input
+        id={id}
+        type="text"
+        className="px-3 py-2 font-bold rounded-lg outline-none"
+        {...props}
+      />
     </p>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,9 +3,11 @@ import Input from "../components/Input";
 const HomePage = () => {
   return (
     <section className="flex items-center justify-center w-full h-screen bg-secondary">
-      <div className="w-[400px] rounded-lg bg-primary px-4 py-8">
+      <div className="w-[400px] rounded-lg bg-primary px-4 py-6">
         <h1 className="mb-10 text-3xl text-center text-main_white">React + Typescript</h1>
-        <Input id="name" label="Your Name" />
+        <Input id="name" label="Your Name" type="text"/>
+        <Input id="age" label="Your Age" type="number"/>
+        <button className="w-full p-3 mt-6 transition-all duration-150 ease-in-out rounded-lg bg-secondary text-main_white hover:bg-light_primary hover:text-primary">submit</button>
       </div>
     </section>
   );
